83eb292e72c8a1b6873323d9a4e2e2a578fda6a8
mmgmgmgmgmgm <97559571+mmgmgmgmgmgm@users.noreply.github.com>
2022-10-22 11:29:57 -0400

    Update link.py

--- link.py
+++ link.py
@@ -8,33 +8,64 @@ init(convert=True)
 
 os.system('cls')
 
-banner1 = '''
-    
-         _______                   _____                    _____                    _____                _____          
-        /::\    \                 /\    \                  /\    \                  /\    \              /\    \         
-       /::::\    \               /::\    \                /::\    \                /::\    \            /::\    \        
-      /::::::\    \             /::::\    \              /::::\    \               \:::\    \           \:::\    \       
-     /::::::::\    \           /::::::\    \            /::::::\    \               \:::\    \           \:::\    \      
-    /:::/~~\:::\    \         /:::/\:::\    \          /:::/\:::\    \               \:::\    \           \:::\    \     
-   /:::/    \:::\    \       /:::/__\:::\    \        /:::/__\:::\    \               \:::\    \           \:::\    \    
-  /:::/    / \:::\    \     /::::\   \:::\    \      /::::\   \:::\    \              /::::\    \          /::::\    \   
- /:::/____/   \:::\____\   /::::::\   \:::\    \    /::::::\   \:::\    \    ____    /::::::\    \        /::::::\    \  
-|:::|    |     |:::|    | /:::/\:::\   \:::\____\  /:::/\:::\   \:::\ ___\  /\   \  /:::/\:::\    \      /:::/\:::\    \ 
-|:::|____|     |:::|    |/:::/  \:::\   \:::|    |/:::/__\:::\   \:::|    |/::\   \/:::/  \:::\____\    /:::/  \:::\____\
- \:::\    \   /:::/    / \::/   |::::\  /:::|____|\:::\   \:::\  /:::|____|\:::\  /:::/    \::/    /   /:::/    \::/    /
-  \:::\    \ /:::/    /   \/____|:::::\/:::/    /  \:::\   \:::\/:::/    /  \:::\/:::/    / \/____/   /:::/    / \/____/ 
-   \:::\    /:::/    /          |:::::::::/    /    \:::\   \::::::/    /    \::::::/    /           /:::/    /          
-    \:::\__/:::/    /           |::|\::::/    /      \:::\   \::::/    /      \::::/____/           /:::/    /           
-     \::::::::/    /            |::| \::/____/        \:::\  /:::/    /        \:::\    \           \::/    /            
-      \::::::/    /             |::|  ~|               \:::\/:::/    /          \:::\    \           \/____/             
-       \::::/    /              |::|   |                \::::::/    /            \:::\    \                              
-        \::/____/               \::|   |                 \::::/    /              \:::\____\                             
-         ~~                      \:|   |                  \::/____/                \::/    /                             
-                                  \|___|                   ~~                       \/____/                              
+colorama.init()
+    print('')
+    print('')
+    print(f'                                       {Fore.LIGHTMAGENTA_EX} $$$$$$\  $$\           $$\                  ')
+    print(f'                                       $$  __$$\ $$ |          $$ |                
+    print(f'                                       $$ /  $$ |$$ | $$$$$$\  $$$$$$$\   $$$$$$\ 
+    print(f'                                       $$$$$$$$ |$$ |$$  __$$\ $$  __$$\  \____$$\ 
+    print(f'                                       $$  __$$ |$$ |$$ /  $$ |$$ |  $$ | $$$$$$$ |
+    print(f'                                       $$ |  $$ |$$ |$$ |  $$ |$$ |  $$ |$$  __$$ |
+    print(f'                                       $$ |  $$ |$$ |$$$$$$$  |$$ |  $$ |\$$$$$$$ |
+    print(f'{Fore.WHITE}> Made by Ishaan{Fore.RESET} {Fore.LIGHTMAGENTA_EX}'\__|  \__|\__|$$  ____/ \__|  \__| \_______|
+              $$ |                          
+              $$ |                          
+              \__|   )
+    print(f'{Fore.WHITE}> WIP{Fore.RESET}') 
+    print(f'{Fore.LIGHTMAGENTA_EX}────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────{Fore.RESET}')
+    print(
+        f'{Fore.LIGHTMAGENTA_EX}[1]{Fore.RESET} DO NOT USE       {Fore.BLACK}|{Fore.RESET}{Fore.LIGHTMAGENTA_EX}[9]{Fore.RESET} DO NOT USE     {Fore.BLACK}|{Fore.RESET}{Fore.LIGHTMAGENTA_EX}[17]{Fore.RESET} DO NOT USE{Fore.RESET}')
+    print(
+        f'{Fore.LIGHTMAGENTA_EX}[2]{Fore.RESET} DO NOT USE      {Fore.BLACK}|{Fore.RESET}{Fore.LIGHTMAGENTA_EX}[10]{Fore.RESET} DO NOT USE     {Fore.BLACK}|{Fore.RESET}{Fore.LIGHTMAGENTA_EX}[18]{Fore.RESET} DO NOT USE{Fore.RESET}')
+    print(
+        f'{Fore.LIGHTMAGENTA_EX}[3]{Fore.RESET} DO NOT USE      {Fore.BLACK}|{Fore.RESET}{Fore.LIGHTMAGENTA_EX}[11]{Fore.RESET} DO NOT USE     {Fore.BLACK}|{Fore.RESET}{Fore.LIGHTMAGENTA_EX}[19]{Fore.RESET} DO NOT USE{Fore.RESET}')
+    print(
+        f'{Fore.LIGHTMAGENTA_EX}[4]{Fore.RESET} DO NOT USE      {Fore.BLACK}|{Fore.RESET}{Fore.LIGHTMAGENTA_EX}[12]{Fore.RESET} DO NOT USE     {Fore.BLACK}|{Fore.RESET}{Fore.LIGHTMAGENTA_EX}[20]{Fore.RESET} DO NOT USE{Fore.RESET}')
+    print(
+        f'{Fore.LIGHTMAGENTA_EX}[5]{Fore.RESET} DO NOT USE      {Fore.BLACK}|{Fore.RESET}{Fore.LIGHTMAGENTA_EX}[13]{Fore.RESET} DO NOT USE     {Fore.BLACK}|{Fore.RESET}{Fore.LIGHTMAGENTA_EX}[21]{Fore.RESET} DO NOT USE{Fore.RESET}')
+    print(
+        f'{Fore.LIGHTMAGENTA_EX}[6]{Fore.RESET} DO NOT USE      {Fore.BLACK}|{Fore.RESET}{Fore.LIGHTMAGENTA_EX}[14]{Fore.RESET} DO NOT USE     {Fore.BLACK}|{Fore.RESET}{Fore.LIGHTMAGENTA_EX}[22]{Fore.RESET} DO NOT USE{Fore.RESET}')
+    print(
+        f'{Fore.LIGHTMAGENTA_EX}[7]{Fore.RESET} DO NOT USE      {Fore.BLACK}|{Fore.RESET}{Fore.LIGHTMAGENTA_EX}[15]{Fore.RESET} DO NOT USE     {Fore.BLACK}|{Fore.RESET}{Fore.LIGHTMAGENTA_EX}[23]{Fore.RESET} About {Fore.RESET}')
+    print(
+        f'{Fore.LIGHTMAGENTA_EX}[8]{Fore.RESET} DO NOT USE      {Fore.BLACK}|{Fore.RESET}{Fore.LIGHTMAGENTA_EX}[16]{Fore.RESET} DO NOT USE     {Fore.BLACK}|{Fore.RESET}{Fore.LIGHTMAGENTA_EX}[24]{Fore.RESET} {Fore.LIGHTRED_EX}Exit{Fore.RESET} {Fore.RESET}')
+
+    print(f'{Fore.LIGHTMAGENTA_EX}────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────{Fore.RESET}')
+    choice = input('[(Currently only 23 works)>>>]')
+
+    if choice == '23':
+        print(f'''{Fore.WHITE} Hello, just a quick message talking about this nuker!
+- This "GANG Multi Tool was created / dev by Ishaan
+- This is a work in progress
+- 12 years old and mainly code with python
+- Thanks for using my tool!
+- if you enjoy this make sure to star it and follow my github!
+{Fore.RESET}''')
+
+
+        exit = input('press any key: ')
+        exit = clear()
+        exit = spammer()
+
+
+    if choice == '24':
+        os.system('exit')
+      
+               
                                                                                                                          
   
 
-        '''
-Nut = "        Program is \nmade by Ishaan, Thanks to Ajit Mohapatra for the support!"
+       
  
 Write.Print(Add.Add(banner1, Nut, 4), Colors.blue_to_green, interval=0.0.0001)
